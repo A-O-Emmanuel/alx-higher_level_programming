4df81fd67cf38893a05ea85c2bc6e537a8a6b0f4
A-O-Emmanuel <emmanuel0058@gmail.com>
2022-12-01 14:04:53 -0800

    2-print_alphabet.py

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Obi Akwari Obinna Emmanuel
+
+"""print numbers 0 to 98 in decimal and hexadecimal."""
+for number in range(0, 99):
+    print("{} = {}",format(number, hex(number)))
